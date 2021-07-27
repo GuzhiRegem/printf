dbfcf1a2e2d21af0103a1740e9aeb2c944e44799
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-07-26 23:12:18 -0300

    Update functions3.c

--- functions3.c
+++ functions3.c
@@ -90,7 +90,27 @@ int print_ROT(char *p)
  */
 int print_long_int(long int *p)
 {
-	if (*p < 0)
-		_putchar('a');
-	return (0);
+	long int out = 0;
+	long int num;
+	num = *p;
+
+	if (num >= 0)
+	{
+		if (num < 10)
+		{
+			_putchar(num + '0');
+			return (1);
+		}
+		else
+		{
+			out = (_print_int(num / 10) + 1);
+			_putchar((num % 10) + '0');
+			return (out);
+		}
+	}
+	else
+	{
+		_putchar('-');
+		return (_print_int(num * -1) + 1);
+	}
 }
